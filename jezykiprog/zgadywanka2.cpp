3aeb799a736b552303b4529bcb7d4edb0ad7dec2
twofacefil11 <twofacefil11@gmail.com>
2023-11-22 22:47:31 +0000

    Invalid DateTime

--- jezykiprog/zgadywanka2.cpp
+++ jezykiprog/zgadywanka2.cpp
@@ -12,25 +12,27 @@ int main() {
     cout << "Podaj jakis wyraz (max 20 liter): ";
     cin >> wyraz;
 
-    while (wyraz[len] != '\0')
+    while (wyraz[len] != '\0') 
         len++;
 
     system("cls");
 
     while (dobrze == 0) {
+
         if (proby == len) {
             cout << "Nie udalo ci sie zgadnonc. Wyraz to: " << wyraz << endl;
             return 0;
         }
+
         proby++;
 
         cout << "Wraz ma " << len << " liter. Wskazowka: ";
-        for (int i = 0; i != j; i++) {
+
+        for (int i = 0; i != j; i++)
             cout << wyraz[i];
-        }
+
         cout << "\nZgaduj: ";
         cin >> guess;
-
         j++;
 
         for (int x = 0; wyraz[x] == guess[x]; x++) {

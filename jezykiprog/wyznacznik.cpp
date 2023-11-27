8d5c2105a092c4b4130b8c7bc662dcdc6943de35
twofacefil11 <twofacefil11@gmail.com>
2023-11-27 23:31:29 +0000

    Invalid DateTime

--- jezykiprog/wyznacznik.cpp
+++ jezykiprog/wyznacznik.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int main(void) {
 
     int size = 3;
+
     float arr[size][size];
 
     for (int i = 0; i < size; i++) {
@@ -14,15 +15,28 @@ int main(void) {
         }
     }
 
-    for (int i = 0;i < size; i++) {
+    for (int i = 0; i < size; i++) {
         for (int j = 0; j < size; j++) {
             cout << arr[i][j] << " ";
         }
         cout << endl;
+    } 
+
+    float dl, dr;
+    int offset = 0;
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+
+        }
     }
-    
 
 
+    //for evry row iterate twice
+        //once to add multiplies of diagonalsj to right
+            //to do thatjust have an inside loop that adds one every time to te rows
+            //for exciding rows, just subtract the size from its index and all gonna be fine;
+        //once to subtract multiples fo diagonals to left
 
+//or can i make it in one loop for both diagonals ?
     return 0;
 }

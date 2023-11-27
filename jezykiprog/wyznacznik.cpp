82b3ede9bd7f5b2f0b4488363ec0026f3b6abd4e
twofacefil11 <twofacefil11@gmail.com>
2023-11-27 22:12:40 +0000

    Invalid DateTime

--- jezykiprog/wyznacznik.cpp
+++ jezykiprog/wyznacznik.cpp
@@ -2,21 +2,27 @@
 
 using namespace std;
 
-int main(void)
-{
+int main(void) {
 
     int size = 3;
-
     float arr[size][size];
 
-    for (int i = 0; i < size; i++)
-    {
-        for (int j = 0; i < size; i++)
-        {
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
             cout << "Wklep wartość elementu [" << i << "][" << j << "]";
             cin >> arr[i][j];
         }
     }
+
+    for (int i = 0;i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+    
+
+
 
     return 0;
 }

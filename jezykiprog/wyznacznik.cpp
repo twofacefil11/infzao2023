1e59fdf0d7b202859953448d90f98046a1b5e075
twofacefil11 <twofacefil11@gmail.com>
2023-11-28 00:16:52 +0000

    Invalid DateTime

--- jezykiprog/wyznacznik.cpp
+++ jezykiprog/wyznacznik.cpp
@@ -10,7 +10,7 @@ int main(void) {
 
     for (int i = 0; i < size; i++) {
         for (int j = 0; j < size; j++) {
-            cout << "Wklep wartość elementu [" << i << "][" << j << "]";
+            cout << "Wklep wartocs elementu [" << i << "][" << j << "]: ";
             cin >> arr[i][j];
         }
     }

a78036a88e01a9ab2f6cb86f275f20c8b5a8ce2d
twofacefil11 <twofacefil11@gmail.com>
2023-11-28 02:00:55 +0000

    Invalid DateTime

--- jezykiprog/montecarlo.cpp
+++ jezykiprog/montecarlo.cpp
@@ -1,70 +1,36 @@
-// #include <iostream>
-// #include <time.h>
-
-// using namespace std;
-
-// int main(void) {
-//     srand(time(NULL));
-
-//     long PREC = 100000;
-//     long p;
-//     long x, y;
-//     long dystans;
-//     long w = 0, total = 0;
-//     double pi;
-
-//     cout << "Podaj liczbe punkuf: ";
-//     cin >> p;  
-//     p++;
-
-//     for (int i = 0; i < p; i++) {
-        
-//         x = double(rand() % PREC + 1) / PREC;
-//         y = double(rand() % PREC + 1) / PREC;
-    
-//         dystans = x * x + y * y;
-
-//         if (dystans <= 1)
-//             w++;
-//         total++;
-
-//     }
-//     pi = double(4 * w) / total;
-
-//     cout << "Masz tu PI: " << pi << endl;
-//     return 0;
 #include <iostream>
-#include <ctime>
+#include <time.h>
 
 using namespace std;
 
 int main(void) {
     srand(time(NULL));
 
-    const long PREC = 100000;
+    long PREC = 100000;
     long p;
     long x, y;
-    int dystans;
+    long dystans;
     long w = 0, total = 0;
     double pi;
 
-    cout << "Podaj liczbe punktow: ";
+    cout << "Podaj liczbe punkuf: ";
     cin >> p;  
     p++;
 
     for (int i = 0; i < p; i++) {
+        
         x = double(rand() % PREC + 1) / PREC;
         y = double(rand() % PREC + 1) / PREC;
     
         dystans = x * x + y * y;
 
-        if (dystans <= 1.0) // Corrected condition
+        if (dystans <= 1.0)
             w++;
         total++;
-    }
 
+    }
     pi = double(4 * w) / total;
 
     cout << "Masz tu PI: " << pi << endl;
     return 0;
-}
+

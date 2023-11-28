79a92008f19e6967fb7d9db7bbe51e8426f9646c
twofacefil11 <twofacefil11@gmail.com>
2023-11-28 02:43:29 +0000

    Invalid DateTime

--- jezykiprog/montecarlo.cpp
+++ jezykiprog/montecarlo.cpp
@@ -8,7 +8,7 @@ int main(void) {
     srand(time(NULL));
 
     long w = 0, total = 0;
-    long PREC = 10000;
+    long PREC = 100000;
     unsigned long p; //18446744073709551615 (maxa ale max możliwości to RPEC do kwadratu)
     double dystans;
     double x, y;

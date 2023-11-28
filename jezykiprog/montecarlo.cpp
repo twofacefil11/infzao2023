dd40ec3fe39cd3db87afd0a4914123915c858d59
twofacefil11 <twofacefil11@gmail.com>
2023-11-28 02:28:47 +0000

    Invalid DateTime

--- jezykiprog/montecarlo.cpp
+++ jezykiprog/montecarlo.cpp
@@ -1,6 +1,47 @@
+// #include <iostream>
+// #include <time.h>
+
+// using namespace std;
+
+// int main(void) {
+//     srand(time(NULL));
+
+//     long w = 0, total = 0;
+//     long PREC = 1000000;
+//     unsigned long p; //18446744073709551615 (maxa ale max możliwości to RPEC do kwadratu)
+//     double dystans;
+//     double x, y;
+//     double pi;
+
+//     cout << "Podaj liczbe punkuf: ";
+//     cin >> p;  
+//     p++;
+
+//     for (unsigned long i = 0; i < p; i++) {
+        
+//         x = double(rand() % (PREC + 1)) / PREC;
+//         y = double(rand() % (PREC + 1)) / PREC;
+    
+//         dystans = x * x + y * y;
+
+//         if (dystans <= 1.0)
+//             w++;
+//         total++;
+
+//         cout << " " << x << " " << y << " " << w << " " << total << endl;
+//         cout << dystans << endl;
+//     }
+//     pi = double(4 * w) / total;
+//     cout << "Masz tu PI: " << pi << endl;
+
+
+//     return 0;
+// }
+
+
 #include <iostream>
-#include <time.h>
-#include <math.h>
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
 
@@ -9,34 +50,29 @@ int main(void) {
 
     long w = 0, total = 0;
     long PREC = 1000000;
-    unsigned long p; //18446744073709551615 (maxa ale max możliwości to RPEC do kwadratu)
+    unsigned long p;
     double dystans;
     double x, y;
     double pi;
 
-    cout << "Podaj liczbe punkuf: ";
-    cin >> p;  
+    cout << "Podaj liczbe punktów: ";
+    cin >> p;
     p++;
 
     for (unsigned long i = 0; i < p; i++) {
-        
         x = double(rand() % (PREC + 1)) / PREC;
         y = double(rand() % (PREC + 1)) / PREC;
-    
-        dystans = sqrt(x * x + y * y);
 
-        if (dystans <= 1.0)
+        dystans = x * x + y * y;
+
+        if (dystans <= 1.0 && x >= 0 && y >= 0)
             w++;
         total++;
 
         cout << " " << x << " " << y << " " << w << " " << total << endl;
-        cout << dystans << endl;
     }
     pi = double(4 * w) / total;
-    cout << "Masz tu PI: " << pi << endl;
-
+    cout << "Masz tu PI (estymacja tylko na jednej ćwiartce): " << pi << endl;
 
     return 0;
 }
-
-

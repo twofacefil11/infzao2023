8711c10b9ed7f0ba56fab4ad560d8fbe4413a0ab
twofacefil11 <twofacefil11@gmail.com>
2023-11-28 01:28:02 +0000

    Invalid DateTime

--- jezykiprog/montecarlo.cpp
+++ jezykiprog/montecarlo.cpp
@@ -6,5 +6,13 @@ using namespace std;
 int main(void) {
     srand(time(NULL));
 
+    long p;
+
+    cout << "Podaj liczbe punkuf: ";
+    cin >> p;
+
+
+    for (int i = 0; i < )
+
     return 0;
 }
